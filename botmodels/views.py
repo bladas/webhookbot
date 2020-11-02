6135a10be03866fba8cb49fdfe94ab3c59ee3183
bladas <dashkevich_v@ukr.net>
2020-11-02 19:31:47 +0200

    save request.data

--- botmodels/views.py
+++ botmodels/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from  .models import Customer,Message
 from telegram import Bot
 import json
 
-class BotView(APIView):
-    def post(self,request):
-        r = request.data
-        print(r)
-        return Response('Ok',status = 200)
 
+class BotView(APIView):
+    def post(self, request):
+        r = request.data
+        c = Customer.objects.filter(pk = 1)
+        Message.objects.create(
+            text = r,
+            customer=c
+        )
+        return Response('Ok', status=200)
 
     def get(self, request):
-        return Response('Fuck u',status=200)
+        return Response('Ok', status=200)
