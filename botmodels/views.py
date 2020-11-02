10775b0e5d65100bb5673bff1fb730df9d3232b7
bladas <dashkevich_v@ukr.net>
2020-11-02 19:45:33 +0200

    save user and message

--- botmodels/views.py
+++ botmodels/views.py
@@ -9,12 +9,21 @@ import json
 
 class BotView(APIView):
     def post(self, request):
-        r = request.data
-        c = Customer.objects.get(pk = 1)
-        Message.objects.create(
-            text = r,
+        r = request.data['message']
+        chat_id = r['chat']['id']
+        customer_name = r['chat']['first_name'] + ' ' + r['chat']['last_name']
+        text = r['text']
+        c, _ = Customer.objects.get_or_create(
+            customer_id=chat_id,
+            defaults={
+                'name':customer_name
+            }
+        )
+        add_message = Message.objects.create(
+            text=text,
             customer=c
         )
+
         return Response('Ok', status=200)
 
     def get(self, request):
