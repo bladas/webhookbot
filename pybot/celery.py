927a29678af92ebb78e28aa9e89b9ad05312f6f9
bladas <dashkevich_v@ukr.net>
2020-12-10 05:39:23 +0200

    sdasd

--- pybot/celery.py
+++ pybot/celery.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program
@@ -11,6 +12,15 @@ app = Celery('pybot')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 7:30 a.m.
+    'add-every-minute': {
+        'task': 'botmodels.tasks.periodic_send',
+        'schedule': crontab(minute=1),
+        'args':"",
+    },
+}
 
 @app.task(bind=True)
 def debug_task(self):
